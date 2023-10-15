b96f8ad41bc2cbd1f22bfff6dc0ebecb842e79a0
jiacheng <jd588@njit.edu>
2023-10-15 16:45:19 -0400

    Merge remote-tracking branch 'origin/main'

---
+++ copy_txt_file_to_operate.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+# 指定bugreport文件夹的路径
+bugreport_folder = "/home/jiacheng/PycharmProjects/bug_report_webcrawler/bugreport"
+
+# 遍历bugreport文件夹下的所有子文件夹
+for subdir, dirs, files in os.walk(bugreport_folder):
+    for file in files:
+        # 检查文件是否为txt文件
+        if file.endswith('.txt'):
+            original_file_path = os.path.join(subdir, file)
+            # 创建新文件名（加上'copy'后缀）
+            new_file_name = file.replace('.txt', 'copy.txt')
+            new_file_path = os.path.join(subdir, new_file_name)
+
+            # 复制文件
+            shutil.copy2(original_file_path, new_file_path)
+
+print("复制完成！")
